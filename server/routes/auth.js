8d0eff4aaa173a126c92ea4fe64cd01c08d8fa90
harshittsaini <harshitsaini61@gmail.com>
2024-10-12 16:28:36 +0530

    updated auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -65,8 +65,7 @@ router.post("/login", async (req, res) => {
     const options = {
       expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      sameSite: 'None',
-      secure: process.env.NODE_ENV === 'production',  // Enable secure cookies in production
+      sameSite: 'None',  // Allow cross-origin requests with credentials
     };
 
     res.cookie("token", token, options).status(200).json({
