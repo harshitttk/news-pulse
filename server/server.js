5ea30775b7379c9be962f82b5803814440a30731
Harshit Kumar <harshitsaini61@gmail.com>
2024-10-12 15:00:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ app.use(cookieParser());
 //Define cors
 const cors = require('cors');
 app.use(cors({
-  origin: 'http://localhost:5173/',  // Frontend URL
   credentials: true                 // Allows credentials (cookies, headers, etc.)
 }));
 
